610d17af4679aa5bbdb932932569cfeeb4e3faf5
minamino shiho <shelling519@gmail.com>
2025-03-12 02:17:44 +0900

    create UlAm

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { LabelAm } from "./components/atoms/common/LabelAm";
 import { InputAm } from "./components/atoms/common/InputAm";
 import { ListAm } from "./components/atoms/common/ListAm";
+import { UlAm } from "./components/atoms/common/UlAm";
 
 function App() {
   const handleChanged = (v: string) => {
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
-      <ul>
+      <UlAm>
         <ListAm>
           <LabelAm label={"test"}>
             <InputAm placeholder={"入力欄"} changed={handleChanged} />
@@ -21,7 +22,7 @@ function App() {
             <InputAm placeholder={"入力欄"} changed={handleChanged} />
           </LabelAm>
         </ListAm>
-      </ul>
+      </UlAm>
     </>
   );
 }
